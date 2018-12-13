9b476116b83023d684d6251e184d425059e94273
Matti Paksula <matti.paksula@iki.fi>
2018-12-13 02:22:03 +0200

    Mouse.move with hash as primary

--- lib/jaguar/dsl/mouse.rb
+++ lib/jaguar/dsl/mouse.rb
@@ -2,14 +2,22 @@ module Jaguar
   module DSL
     module Mouse
       include Contracts::Core
-      Contract Jaguar::C::Num, Jaguar::C::Num => self.class
-      def self.move(x,y)
+
+      Contract Jaguar::C::KeywordArgs[
+        x: Integer, y: Integer
+      ] => self.class
+      def self.move(x:nil, y:nil)
         Jaguar.driver.do({
           device: :mouse,
           action: :move,
           params: { x: x, y: y }
         })
         self
+      end
+
+      Contract Integer, Integer => self.class
+      def self.move(x,y)
+        move x:x, y:y
       end
     end
   end
